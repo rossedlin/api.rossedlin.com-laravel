54b5331e75ddf557994b6c551a2870337e25806b
Ross Edlin <rossedlin@gmail.com>
2018-02-15 14:37:43 +0000

    add basic send template

---
+++ resources/views/emails/basic/send.blade.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 13/02/2018
+ * Time: 15:49
+ */
+?>
+<html>
+<head>{{$title}}</head>
+<body style="background: black; color: white">
+<h1>{{$title}}</h1>
+<p>{{$content}}</p>
+</body>
+</html>
