060ccf427c2d20d5defe2610f8a2a20f3891eb18
Ross Edlin <rossedlin@gmail.com>
2018-02-22 11:03:37 +0000

    add - test/post

---
+++ app/Http/Controllers/Test/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use \App\Http\Controllers;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 22/02/18
+ * Time: 10:54
+ */
+class Post extends Controllers\_Api
+{
+    /**
+     * @return array
+     */
+    protected function getPayload()
+    {
+        return [
+            'body' => $this->getRequest()->getContent(),
+        ];
+    }
+}
