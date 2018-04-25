ada5c12f5ae1dda3299ccc15a3427e0241834f9c
Ross Edlin <rossedlin@gmail.com>
2018-04-25 14:23:04 +0100

    add enums for user

---
+++ app/Enums/UserAttributes.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 21/03/18
+ * Time: 09:48
+ */
+
+class UserAttributes
+{
+    const _TABLE = 'users';
+
+    const USERNAME = 'username';
+    const EMAIL    = 'email';
+    const PASSWORD = 'password';
+    const SALT     = 'salt';
+}
