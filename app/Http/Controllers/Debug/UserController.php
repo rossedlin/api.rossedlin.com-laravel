287d7779b87292f3bc3fa4eb95cb6e3439ed6dbf
Ross Edlin <rossedlin@gmail.com>
2018-04-25 14:39:48 +0100

    add user controller

---
+++ app/Http/Controllers/Debug/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Debug;
+
+use App\Http\Controllers\_WebController;
+use App\Model\User;
+use Illuminate\Http\Request;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 21/03/18
+ * Time: 17:55
+ *
+ * Class UserController
+ * @package App\Http\Controllers\Debug
+ */
+class UserController extends _WebController
+{
+    /**
+     * @param Request $request
+     */
+    public function __invoke(Request $request)
+    {
+        $user = new User();
+        $user->username = 'rossedlin';
+        $user->email = "rossedlin@gmail.com";
+        $user->setPassword('FNAm57ehihCX46V7');
+
+        exit;
+    }
+}
