23ad8e0dc1e24d867622e80bfef5ac974b089051
Ross Edlin <rossedlin@gmail.com>
2018-04-25 14:43:12 +0100

    update build script for LIVE

--- build/siteground-live.sh
+++ build/siteground-live.sh
@@ -11,25 +11,29 @@ git clone -b master https://rossedlin@bitbucket.org/rossedlin/api.rossedlin.com.
 
 if [ -d ~/rossedlin.com/api.rossedlin.com-laravel ]; then
 
+    #Permissions
+    chmod 755 -R ~/rossedlin.com/api.rossedlin.com-laravel
+
     #Composer
     cd ~/rossedlin.com/api.rossedlin.com-laravel
     composer install
 
     #Environment File
-#    cd ~/rossedlin.com/
-#    cp ~/rossedlin.com/api/.env ~/rossedlin.com/api.rossedlin.com-laravel/.env
+    cd ~/rossedlin.com/
+    cp ~/rossedlin.com/laravel-api/.env ~/rossedlin.com/api.rossedlin.com-laravel/.env
 
     #Folders
-#    cd ~/rossedlin.com/
-#    mv api old_api
-#    mv api.rossedlin.com-laravel api
+    cd ~/rossedlin.com/
+    mv laravel-api old-laravel-api
+    mv api.rossedlin.com-laravel laravel-api
 
-#    #Public
-#    cd ~/rossedlin.com/www
-#    cp ./build/live/robots.txt ./public/robots.txt
-#    cp ./build/live/.htaccess ./public/.htaccess
+    #Public
+    cd ~/rossedlin.com/laravel-api
+    cp ./build/live/robots.txt ./public/robots.txt
+    cp ./build/live/.htaccess ./public/.htaccess
 #
     #Permissions
-#    chmod 755 -R ~/rossedlin.com/api
+    cd ~/rossedlin.com
+    chmod 755 -R ~/rossedlin.com/laravel-api
 
 fi
