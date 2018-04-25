0e06f5c4c5fcac52507c493051cbfc9cbb9a3979
Ross Edlin <rossedlin@gmail.com>
2018-04-25 14:22:51 +0100

    add enums for heartbeat

---
+++ app/Enums/Heartbeat/EntityAttributes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums\Heartbeat;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 21/03/18
+ * Time: 09:48
+ */
+
+class EntityAttributes
+{
+    const _TABLE = 'heartbeat_entity';
+}
---
+++ app/Enums/Heartbeat/PulseAttributes.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Enums\Heartbeat;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 21/03/18
+ * Time: 09:48
+ */
+
+class PulseAttributes
+{
+    const _TABLE = 'heartbeat_pulse';
+}
