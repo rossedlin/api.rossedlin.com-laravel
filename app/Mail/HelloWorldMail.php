1932cd80d458a4271f9c74b9a507e8701b8fa2f7
Ross Edlin <rossedlin@gmail.com>
2018-04-26 15:14:15 +0100

    add phpdocs

--- app/Mail/HelloWorldMail.php
+++ app/Mail/HelloWorldMail.php
@@ -7,6 +7,17 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
+/**
+ * Created by PhpStorm.
+ *
+ * @author  Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 26/04/2018
+ * Time: 14:21
+ *
+ * Class HelloWorldMail
+ * @package App\Mail
+ */
 class HelloWorldMail extends Mailable
 {
 //    use Queueable, SerializesModels;
