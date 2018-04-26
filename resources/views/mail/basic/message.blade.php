be47d4c3a3d8bcb9477ddb36efeb7ad8567ecce8
Ross Edlin <rossedlin@gmail.com>
2018-04-26 15:14:00 +0100

    add basic-message

---
+++ resources/views/mail/basic/message.blade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 13/02/2018
+ * Time: 15:49
+ *
+ * @var string $content
+ */
+?>
+<html>
+<head>Ross Edlin</head>
+<body style="background: black; color: white">
+<p><?= $content ?></p>
+</body>
+</html>
