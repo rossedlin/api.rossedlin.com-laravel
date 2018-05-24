fca8e0682a9715b1e12e9b76d8e7648f029ed2f7
Ross Edlin <rossedlin@gmail.com>
2018-05-24 13:43:00 +0100

    remove new line

--- app/Http/Controllers/Heartbeat/SendPulseController.php
+++ app/Http/Controllers/Heartbeat/SendPulseController.php
@@ -45,7 +45,6 @@ class SendPulseController extends Base\ApiController
             $entity->save();
         }
 
-
         $pulse                      = new HeartbeatPulse();
         $pulse->heartbeat_entity_id = $entity->id;
         $pulse->ip_address          = $this->getRequest()->ip();
