e58cf122adf88ceceea67d2c2606ae9820074490
Ross Edlin <rossedlin@gmail.com>
2018-05-24 13:40:46 +0100

    add data + data_type to send pulse

--- app/Http/Controllers/Heartbeat/SendPulseController.php
+++ app/Http/Controllers/Heartbeat/SendPulseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Heartbeat;
 
+use App\Exceptions\ApiException;
 use \App\Http\Controllers\Base;
 use \App\Models\HeartbeatEntity;
 use \App\Models\HeartbeatPulse;
@@ -22,12 +23,15 @@ class SendPulseController extends Base\ApiController
 {
     /**
      * @return array
+     * @throws ApiException
      */
     protected function getPayload()
     {
         $new_entity     = false;
         $recorded_pulse = false;
         $code           = $this->getRequest()->post('code');
+        $data           = $this->getRequest()->post('data');
+        $data_type      = $this->getRequest()->post('data_type');
 
         /**
          * @var \App\Models\HeartbeatEntity $entity
@@ -45,17 +49,48 @@ class SendPulseController extends Base\ApiController
         $pulse->heartbeat_entity_id = $entity->id;
         $pulse->ip_address          = $this->getRequest()->ip();
 
+        /**
+         * Handle Data
+         */
+        if ($data !== null) {
+            $data_type_int = $this->isDataTypeValid($data_type);
+
+            $pulse->data      = $data;
+            $pulse->data_type = $data_type_int;
+        }
+
+        /**
+         * Save Pulse
+         */
         if ($pulse->save()) {
             $recorded_pulse = true;
         }
 
         return [
             'code'           => $code,
-            'alive'         => true,
+            'alive'          => true,
             'recorded_pulse' => $recorded_pulse,
             'new_entity'     => $new_entity,
             'entity'         => $entity,
             'pulse'          => $pulse,
         ];
+    }
+
+    /**
+     * @param $data_type
+     *
+     * @return int
+     * @throws ApiException
+     */
+    private function isDataTypeValid($data_type): int
+    {
+        switch ($data_type) {
+            case 'json':
+                return 1;
+            case 'xml':
+                return 2;
+        }
+
+        throw new ApiException("Invalid Data Type");
     }
 }
