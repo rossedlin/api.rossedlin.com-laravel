020b8b04f19a65b99938f688028a94a2594d44ed
Ross Edlin <rossedlin@gmail.com>
2020-12-13 17:37:34 +0700

    add cache clearing on start

--- up.sh
+++ up.sh
@@ -2,3 +2,7 @@
 
 docker-compose down
 docker-compose up -d
+
+docker exec api_web_1 php artisan route:clear
+docker exec api_web_1 php artisan config:clear
+docker exec api_web_1 php artisan cache:clear
